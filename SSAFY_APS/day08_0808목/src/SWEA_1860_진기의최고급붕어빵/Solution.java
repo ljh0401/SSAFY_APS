1b26afc3d11386d2b10c4e661535ef641172d476
ljh0401 <dlwnsghks0401@gmail.com>
2024-08-08 20:19:25 +0900

    08.08. (백만장자 추가 고민 중)

--- SSAFY_APS/day08_0808목/src/SWEA_1860_진기의최고급붕어빵/Solution.java
+++ SSAFY_APS/day08_0808목/src/SWEA_1860_진기의최고급붕어빵/Solution.java
@@ -8,37 +8,67 @@ class Solution
     {
          
         Scanner sc = new Scanner(System.in);
+        
+        // test_case 입력
         int T;
         T=sc.nextInt();
+        
+        // for A 라벨, test 수 만큼 반복 
         A:
         for(int test_case = 1; test_case <= T; test_case++)
         {
+        	
+        	// N, M, K 입력 받기
             int N = sc.nextInt();
             int M = sc.nextInt();
             int K = sc.nextInt();
+            
+            // 사람이 최대한 늦게 오는 것이 111111이므로 111112로 배열 생성
             int[] persons = new int[11112];
+            
+            // 시간 별로 오는 사람 수 체크
             for (int i = 0; i < N; i++) {
                 persons[sc.nextInt()] += 1;
             }
+            
+            // 붕어빵 수 boo, 맛있게 먹은 사람 수 yam 설정
             int boo = 0;
             int yam = 0;
+            
+            // 일단 진기는 모든지 possible
             String jingi = "Possible";
+            
+            // 붕어빵을 만들어보면서
             for (int i = 0; i <= 11111; i++){
+            	
+            	// 주어진 시간이 지날 때마다 붕어빵 개수 추가
                 if (i != 0 & i%M == 0) {
                     boo += K;
                 }
+                
+                // 만약 오는 사람이 없으면 그냥 넘어감
                 if (persons[i] == 0) continue;
+                
+                // 있다면 붕어빵을 팔려고 시도
                 boo -= persons[i];
+                
+                // 만약 붕어빵이 부족하면 진기는 불가능함
                 if (boo < 0) {
                     jingi = "Impossible";
                     System.out.println("#"+test_case+" "+jingi);
                     continue A;
                 }
+                
+                // 잘 팔았으면 yam한 사람 추가
                 else {
                     yam += persons[i];
                 }
+                
+                // 만약 모두가 yam하면 장사 끝
                 if (yam >= N) break;
             }
+            
+            // 장사가 가능했는지 출력
             System.out.println("#"+test_case+" "+jingi);
         }
     }
--- SSAFY_APS/day08_0808목/src/SWEA_1974_스도쿠검증/Solution.java
+++ SSAFY_APS/day08_0808목/src/SWEA_1974_스도쿠검증/Solution.java
@@ -3,37 +3,68 @@ package SWEA_1974_스도쿠검증;
 import java.util.Scanner;
 import java.util.Arrays;
  
+/**	2805 농작물 문제와 같이 입력한 결과를 저장하지 아니하고 최대한 풀어보고자 노력한 결과 너무 긴 스파게티가 나와버렸습니다. ㅜㅜ
+ * 	간단하게 사용한 원리를 설명하겠습니다.
+ * 	
+ * 	우선 1~9까지의 합과 곱이 모두 동일한 경우는 1가지 특이 케이스(이 때 해당 숫자열엔 3이 존재하지 않아 합과 곱이 동일할 때
+ * 	스도쿠가 올바른지 판단하기 위해 3의 유무를 검사합니다.)를 제외하면 없다는 점에 착안, 합과 곱을 가지고 가로, 세로, 3*3 행렬을 검사 했습니다.
+ * 
+ * 이 때 가로는 9줄 모두, 세로는 8줄만 (1~9가 총 9개씩 있으므로 8개만 검사하면 나머지는 자동 입증), 
+ * 3*3 행렬은 4개만 (9개 중 큰 한 줄에 2개씩만 검사하면 나머지 1개는 자동으로 입증) 검사하여 스도쿠인지를 판별하였습니다.
+ * 
+ */
+
 class Solution
 {
     public static void main(String args[]) throws Exception
     {
+    	
+    	// test case 입력 받기
         Scanner sc = new Scanner(System.in);
         int T;
         T=sc.nextInt();
+        
+        // A로 loop 라벨, test 수 만큼 loop
         A:
         for(int test_case = 1; test_case <= T; test_case++)
         {
             int ans = 1;
+            
+            // 세로줄 검사 위한 배열 생성
             int[] sum = new int[8];
             int[] pro = new int[8];
             Arrays.fill(pro, 1);
             boolean[] B = new boolean[9];
+            
+            // 3*3 검사 위한 배열 생성
             int[] SSum = new int[4];
             int[] PPro = new int[4];
             Arrays.fill(PPro, 1);
             boolean[] BB = new boolean[4];
+            
+            // 입력 + 가로줄 검사 + 검사 위한 세로줄, 3*3 행렬 수 입력
             for (int i = 0 ; i < 9 ; i ++) {
+            	
+            	// 가로줄 검사 위한 변수 설정
                 boolean three = false;
                 int S = 0;
                 int P = 1;
+                
+                // 숫자 입력 받으면서 필요한 곳에 모두 입력
                 for (int j = 0; j < 9; j++) {
                     int num = sc.nextInt();
+                    
+                    // 3이 나올 경우 가로, 세로 줄에 3이 나왔음을 입력
                     if (num == 3) {
                         three = true;
                         B[j] = true;
                     }
+                    
+                    // 가로줄 합, 곱 추가
                     S += num;
                     P *= num;
+                    
+                    // 3*3 행렬 판단 위한 값 추가
                     if ( i < 3 && j < 3) {
                         SSum[0] += num;
                         PPro[0] *= num;
@@ -54,10 +85,14 @@ class Solution
                         PPro[3] *= num;
                         if (num == 3) BB[3] = true;
                     }
+                    
+                    // 세로 줄 판단 위한 값 추가 (9 한 줄은 그냥 넘어가도 괜찮으므로 8 continue)
                     if (j == 8) continue;
                     sum[j] += num;
                     pro[j] *= num;
                 }
+                
+                // 이 때 가로줄의 합, 곱이 1~9까지의 합과 곱가 동일하지 않다면, 혹은 동일하더라도 3이 없다면 스도쿠 아님. (전체 입력을 위해 continue A를 하지 않았습니다.)
                 if (S != 45 || P != 362880) {
                     ans = 0;
                 }
@@ -65,11 +100,17 @@ class Solution
                     ans = 0;
                 }
             }
+            
+            // ans = 0 이라면 정답 출력, continue A
             if (ans == 0) {
                 System.out.println("#"+test_case+" "+ans);
                 continue A;
             }
+            
+            // 세로줄 검증
             for (int i = 0; i < 8; i++) {
+            	
+            	// 이 때 세로줄의 합, 곱이 1~9까지의 합과 곱가 동일하지 않다면, 혹은 동일하더라도 3이 없다면 스도쿠 아님.
                 if (sum[i] != 45 || pro[i] != 362880) {
                     ans = 0;
                     System.out.println("#"+test_case+" "+ans);
@@ -81,7 +122,11 @@ class Solution
                     continue A;
                 }
             }
+            
+            // 3*3 행렬 검증
             for (int i = 0; i < 4; i++) {
+            	
+            	// 이 때 행렬의 합, 곱이 1~9까지의 합과 곱가 동일하지 않다면, 혹은 동일하더라도 3이 없다면 스도쿠 아님. (전체
                 if (SSum[i] != 45 || PPro[i] != 362880) {
                     ans = 0;
                     System.out.println("#"+test_case+" "+ans);
@@ -92,7 +137,9 @@ class Solution
                     System.out.println("#"+test_case+" "+ans);
                     continue A;
                 }
-            }           
+            }
+            
+            // 모두 통과했을 경우 1 출력
             System.out.println("#"+test_case+" "+ans);
         }
     }
